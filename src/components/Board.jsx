74d3f12824d59ea5b2a0df6cf61e4597c34354cd
lmiguelcolombo <lmiguelcolombo@gmail.com>
2023-05-10 10:45:04 -0300

    feat: controlling when a square is clicked

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -7,26 +7,33 @@ import "../App.css";
 //   return;
 // }
 
-function Square({ value }) {
-  const [user, setUser] = useState("");
-
-  return <button className="square">{user}</button>;
+function Square({ value, onSquareClick }) {
+  return (
+    <button onClick={onSquareClick} className="square">
+      {value}
+    </button>
+  );
 }
 
 export default function Board() {
   const [squares, setSquares] = useState(Array(9).fill(null));
+  // output: [ [null], [null], ...9x ]
+
+  const handleClick = () => {
+    console.log("clicked!");
+  };
 
   return (
     <div className="board">
-      <Square value={squares[0]} />
-      <Square value={squares[1]} />
-      <Square value={squares[2]} />
-      <Square value={squares[3]} />
-      <Square value={squares[4]} />
-      <Square value={squares[5]} />
-      <Square value={squares[6]} />
-      <Square value={squares[7]} />
-      <Square value={squares[8]} />
+      <Square value={squares[0]} onSquareClick={handleClick} />
+      <Square value={squares[1]} onSquareClick={handleClick} />
+      <Square value={squares[2]} onSquareClick={handleClick} />
+      <Square value={squares[3]} onSquareClick={handleClick} />
+      <Square value={squares[4]} onSquareClick={handleClick} />
+      <Square value={squares[5]} onSquareClick={handleClick} />
+      <Square value={squares[6]} onSquareClick={handleClick} />
+      <Square value={squares[7]} onSquareClick={handleClick} />
+      <Square value={squares[8]} onSquareClick={handleClick} />
     </div>
   );
 }
