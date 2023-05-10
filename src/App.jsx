61df8061affa9566b9f2b339feb40633ddd9d29a
lmiguelcolombo <lmiguelcolombo@gmail.com>
2023-05-10 09:54:47 -0300

    refactor: using squares as buttons instead of divs

--- src/App.jsx
+++ src/App.jsx
@@ -4,15 +4,15 @@ function App() {
   return (
     <div className="App">
       <div className="board">
-        <div className="square"></div>
-        <div className="square"></div>
-        <div className="square"></div>
-        <div className="square"></div>
-        <div className="square"></div>
-        <div className="square"></div>
-        <div className="square"></div>
-        <div className="square"></div>
-        <div className="square"></div>
+        <button className="square">1</button>
+        <button className="square">2</button>
+        <button className="square">3</button>
+        <button className="square">4</button>
+        <button className="square">5</button>
+        <button className="square">6</button>
+        <button className="square">7</button>
+        <button className="square">8</button>
+        <button className="square">9</button>
       </div>
     </div>
   );
