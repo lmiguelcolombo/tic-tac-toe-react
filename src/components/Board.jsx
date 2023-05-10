95ad56b198ef7de8bcd5d5a45b1db35a664d3488
lmiguelcolombo <lmiguelcolombo@gmail.com>
2023-05-10 10:10:09 -0300

    refactor: onClick function created for changing state of the value

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -1,23 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 import "./board.css";
 import "../App.css";
 
 function Square({ value }) {
-  return <button className="square">{value}</button>;
+  const [user, setUser] = useState("");
+
+  const handleClick = () => {
+    setUser("X");
+  };
+
+  return (
+    <button onClick={handleClick} className="square">
+      {user}
+    </button>
+  );
 }
 
 export default function Board() {
   return (
     <div className="board">
-      <Square value={1} />
-      <Square value={2} />
-      <Square value={3} />
-      <Square value={4} />
-      <Square value={5} />
-      <Square value={6} />
-      <Square value={7} />
-      <Square value={8} />
-      <Square value={9} />
+      <Square />
+      <Square />
+      <Square />
+      <Square />
+      <Square />
+      <Square />
+      <Square />
+      <Square />
+      <Square />
     </div>
   );
 }
